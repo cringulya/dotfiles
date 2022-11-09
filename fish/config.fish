3f6b3c288357d1da1d360a5366580bc6d55bcd1d
Son Artyom <sai@modumlab.com>
2022-11-10 02:54:24 +0300

    added fish conf

---
+++ fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+  alias vi nvim
+  alias py /opt/homebrew/bin/python3
+  alias python3 /opt/homebrew/bin/python3
+  alias python /opt/homebrew/bin/python3
+end
---
+++ fish/functions/_ayu_restore_previous_theme.fish
@@ -0,0 +1,27 @@
+function _ayu_restore_previous_theme
+	set --universal fish_color_autosuggestion d70087
+	set --universal fish_color_cancel --reverse
+	set --universal fish_color_command 00ffd7
+	set --universal fish_color_comment 007B7B
+	set --universal fish_color_cwd green
+	set --universal fish_color_cwd_root red
+	set --universal fish_color_end 02BDBD
+	set --universal fish_color_error 9177E5
+	set --universal fish_color_escape 00a6b2
+	set --universal fish_color_history_current --bold
+	set --universal fish_color_host normal
+	set --universal fish_color_host_remote 
+	set --universal fish_color_keyword 
+	set --universal fish_color_match --background=brblue
+	set --universal fish_color_normal normal
+	set --universal fish_color_operator 00a6b2
+	set --universal fish_color_option 
+	set --universal fish_color_param 4319CC
+	set --universal fish_color_quote 4C3499
+	set --universal fish_color_redirection 248E8E
+	set --universal fish_color_search_match bryellow --background=brblack
+	set --universal fish_color_selection white --bold --background=brblack
+	set --universal fish_color_status red
+	set --universal fish_color_user brgreen
+	set --universal fish_color_valid_path --underline
+end
