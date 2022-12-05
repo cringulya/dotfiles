ac5f9d3704721151ceaa624e990efe7675293ef0
Son Artyom <sai@modumlab.com>
2022-12-05 14:17:52 +0300

    updates

--- lvim/config.lua
+++ lvim/config.lua
@@ -3,108 +3,110 @@ vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 
 -- general
-lvim.log.level = "info"
+lvim.log.level = 'info'
 lvim.format_on_save = {
-	enabled = true,
-	pattern = "*.lua",
-	timeout = 1000,
+  enabled = true,
+  pattern = '*.lua',
+  timeout = 1000,
 }
 
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
 
-lvim.leader = "space"
+lvim.leader = 'space'
 
 -- add your own keymapping
-lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
-lvim.keys.normal_mode["<tab>"] = "<cmd>bn<cr>"
-lvim.keys.normal_mode["<s-tab>"] = "<cmd>bp<cr>"
-lvim.keys.normal_mode["<s-q>"] = "<cmd>bd<cr>"
-lvim.keys.normal_mode["<c-n>"] = "<cmd>NvimTreeToggle<cr>"
-lvim.keys.normal_mode["<esc>"] = "<cmd>cclose<cr>"
-lvim.keys.normal_mode["<c-t>"] = "<cmd>ToggleTerm<cr>"
-lvim.keys.term_mode["<c-t>"] = "<cmd>ToggleTerm<cr>"
-lvim.lsp.buffer_mappings.normal_mode["Ï"] = { vim.lsp.buf.format, "Format" }
-lvim.lsp.buffer_mappings.normal_mode["<leader>r"] = { vim.lsp.buf.rename, "Format" }
+lvim.keys.normal_mode['<C-s>'] = ':w<cr>'
+lvim.keys.normal_mode['<tab>'] = '<cmd>bn<cr>'
+lvim.keys.normal_mode['<s-tab>'] = '<cmd>bp<cr>'
+lvim.keys.normal_mode['<s-q>'] = '<cmd>bd<cr>'
+lvim.keys.normal_mode['<c-n>'] = '<cmd>NvimTreeToggle<cr>'
+lvim.keys.normal_mode['<esc>'] = '<cmd>cclose<cr>'
+lvim.keys.normal_mode['<c-t>'] = '<cmd>ToggleTerm<cr>'
+lvim.keys.term_mode['<c-t>'] = '<cmd>ToggleTerm<cr>'
+lvim.lsp.buffer_mappings.normal_mode['Ï'] = { vim.lsp.buf.format, 'Format' }
+lvim.lsp.buffer_mappings.normal_mode['<leader>r'] =
+  { vim.lsp.buf.rename, 'Format' }
 
-lvim.keys.insert_mode["<c-l>"] = '<CMD>lua require("luasnip").jump(1)<CR>'
-lvim.keys.insert_mode["<c-h>"] = '<CMD>lua require("luasnip").jump(-1)<CR>'
+lvim.keys.insert_mode['<c-l>'] = '<CMD>lua require("luasnip").jump(1)<CR>'
+lvim.keys.insert_mode['<c-h>'] = '<CMD>lua require("luasnip").jump(-1)<CR>'
 
 -- -- Use which-key to add extra bindings with the leader-key prefix
 -- lvim.builtin.which_key.mappings["W"] = { "<cmd>noautocmd w<cr>", "Save without formatting" }
-lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
-lvim.builtin.which_key.mappings["g"]["g"] = { "<cmd>G<cr>", "Fugitive status" }
-lvim.builtin.which_key.mappings["c"] = {}
+lvim.builtin.which_key.mappings['P'] =
+  { '<cmd>Telescope projects<CR>', 'Projects' }
+lvim.builtin.which_key.mappings['g']['g'] = { '<cmd>G<cr>', 'Fugitive status' }
+lvim.builtin.which_key.mappings['c'] = {}
 
 vim.cmd([[
   set langmap=ФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯ;ABCDEFGHIJKLMNOPQRSTUVWXYZ,фисвуапршолдьтщзйкыегмцчня;abcdefghijklmnopqrstuvwxyz
 ]])
 
 -- tabcompletion
-local cmp = require("cmp")
-cmp = require("cmp")
-lvim.builtin.cmp.mapping["<c-j>"] = {}
-lvim.builtin.cmp.mapping["<c-k>"] = {}
-lvim.builtin.cmp.mapping["<Tab>"] = cmp.mapping(function(fallback)
-	if cmp.visible() then
-		local entry = cmp.get_selected_entry()
-		if not entry then
-			cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-			cmp.confirm()
-		else
-			cmp.confirm()
-		end
-	else
-		fallback()
-	end
-end, { "i", "s" })
+local cmp = require('cmp')
+cmp = require('cmp')
+lvim.builtin.cmp.mapping['<c-j>'] = {}
+lvim.builtin.cmp.mapping['<c-k>'] = {}
+lvim.builtin.cmp.mapping['<Tab>'] = cmp.mapping(function(fallback)
+  if cmp.visible() then
+    local entry = cmp.get_selected_entry()
+    if not entry then
+      cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+      cmp.confirm()
+    else
+      cmp.confirm()
+    end
+  else
+    fallback()
+  end
+end, { 'i', 's' })
 
 -- Change theme settings
-lvim.colorscheme = "tokyonight-night"
+lvim.colorscheme = 'tokyonight-night'
 
 -- After changing plugin config exit and reopen LunarVim, Run :PackerSync
 lvim.builtin.alpha.active = true
-lvim.builtin.alpha.mode = "dashboard"
+lvim.builtin.alpha.mode = 'dashboard'
 lvim.builtin.terminal.active = true
-lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.setup.view.side = 'left'
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
-lvim.builtin.project.detection_methods = { "pattern", "lsp" }
+lvim.builtin.project.detection_methods = { 'pattern', 'lsp' }
 lvim.builtin.project.patterns = {
-	".git",
-	".vscode",
-	"build",
-	">polykek",
-	">sandbox",
-	">sonartem",
-	">modumlab",
-	"_darcs",
-	".hg",
-	".bzr",
-	".svn",
-	"Makefile",
-	"package.json",
-	".stylua.toml",
+  '.git',
+  '.vscode',
+  'build',
+  '>polykek',
+  '>sandbox',
+  '>sonartem',
+  '>modumlab',
+  '_darcs',
+  '.hg',
+  '.bzr',
+  '.svn',
+  'Makefile',
+  'package.json',
+  '.stylua.toml',
 }
 
-lvim.builtin.telescope.file_ignore_patterns = { "build" }
+lvim.builtin.telescope.file_ignore_patterns = { 'build' }
 
 lvim.builtin.alpha.dashboard.section.header.val = {
-	"",
-	"⡿⠋⠄⣀⣀⣤⣴⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣌⠻⣿⣿",
-	"⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⠹⣿",
-	"⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠹",
-	"⣿⣿⡟⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡛⢿⣿⣿⣿⣮⠛⣿⣿⣿⣿⣿⣿⡆",
-	"⡟⢻⡇⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣣⠄⡀⢬⣭⣻⣷⡌⢿⣿⣿⣿⣿⣿",
-	"⠃⣸⡀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠈⣆⢹⣿⣿⣿⡈⢿⣿⣿⣿⣿",
-	"⠄⢻⡇⠄⢛⣛⣻⣿⣿⣿⣿⣿⣿⣿⣿⡆⠹⣿⣆⠸⣆⠙⠛⠛⠃⠘⣿⣿⣿⣿",
-	"⠄⠸⣡⠄⡈⣿⣿⣿⣿⣿⣿⣿⣿⠿⠟⠁⣠⣉⣤⣴⣿⣿⠿⠿⠿⡇⢸⣿⣿⣿",
-	"⠄⡄⢿⣆⠰⡘⢿⣿⠿⢛⣉⣥⣴⣶⣿⣿⣿⣿⣻⠟⣉⣤⣶⣶⣾⣿⡄⣿⡿⢸",
-	"⠄⢰⠸⣿⠄⢳⣠⣤⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣼⣿⣿⣿⣿⣿⣿⡇⢻⡇⢸",
-	"⢷⡈⢣⣡⣶⠿⠟⠛⠓⣚⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⢸⠇⠘",
-	"⡀⣌⠄⠻⣧⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠛⠛⠛⢿⣿⣿⣿⣿⣿⡟⠘⠄⠄",
-	"⣷⡘⣷⡀⠘⣿⣿⣿⣿⣿⣿⣿⣿⡋⢀⣠⣤⣶⣶⣾⡆⣿⣿⣿⠟⠁⠄⠄⠄⠄",
-	"⣿⣷⡘⣿⡀⢻⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣿⣿⣿⣿⣷⡿⠟⠉⠄⠄⠄⠄⡄⢀",
-	"⣿⣿⣷⡈⢷⡀⠙⠛⠻⠿⠿⠿⠿⠿⠷⠾⠿⠟⣛⣋⣥⣶⣄⠄⢀⣄⠹⣦⢹⣿",
+  '',
+  '⡿⠋⠄⣀⣀⣤⣴⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣌⠻⣿⣿',
+  '⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⠹⣿',
+  '⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠹',
+  '⣿⣿⡟⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡛⢿⣿⣿⣿⣮⠛⣿⣿⣿⣿⣿⣿⡆',
+  '⡟⢻⡇⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣣⠄⡀⢬⣭⣻⣷⡌⢿⣿⣿⣿⣿⣿',
+  '⠃⣸⡀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠈⣆⢹⣿⣿⣿⡈⢿⣿⣿⣿⣿',
+  '⠄⢻⡇⠄⢛⣛⣻⣿⣿⣿⣿⣿⣿⣿⣿⡆⠹⣿⣆⠸⣆⠙⠛⠛⠃⠘⣿⣿⣿⣿',
+  '⠄⠸⣡⠄⡈⣿⣿⣿⣿⣿⣿⣿⣿⠿⠟⠁⣠⣉⣤⣴⣿⣿⠿⠿⠿⡇⢸⣿⣿⣿',
+  '⠄⡄⢿⣆⠰⡘⢿⣿⠿⢛⣉⣥⣴⣶⣿⣿⣿⣿⣻⠟⣉⣤⣶⣶⣾⣿⡄⣿⡿⢸',
+  '⠄⢰⠸⣿⠄⢳⣠⣤⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣼⣿⣿⣿⣿⣿⣿⡇⢻⡇⢸',
+  '⢷⡈⢣⣡⣶⠿⠟⠛⠓⣚⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⢸⠇⠘',
+  '⡀⣌⠄⠻⣧⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠛⠛⠛⢿⣿⣿⣿⣿⣿⡟⠘⠄⠄',
+  '⣷⡘⣷⡀⠘⣿⣿⣿⣿⣿⣿⣿⣿⡋⢀⣠⣤⣶⣶⣾⡆⣿⣿⣿⠟⠁⠄⠄⠄⠄',
+  '⣿⣷⡘⣿⡀⢻⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣿⣿⣿⣿⣷⡿⠟⠉⠄⠄⠄⠄⡄⢀',
+  '⣿⣿⣷⡈⢷⡀⠙⠛⠻⠿⠿⠿⠿⠿⠷⠾⠿⠟⣛⣋⣥⣶⣄⠄⢀⣄⠹⣦⢹⣿',
 }
 
 -- Automatically install missing parsers when entering buffer
@@ -140,76 +142,78 @@ lvim.builtin.treesitter.auto_install = true
 -- end
 
 -- -- linters and formatters <https://www.lunarvim.org/docs/languages#lintingformatting>
-local formatters = require("lvim.lsp.null-ls.formatters")
+local formatters = require('lvim.lsp.null-ls.formatters')
 formatters.setup({
-	{ command = "stylua" },
-	{ command = "prettier" },
-	{ command = "black" },
+  { command = 'stylua' },
+  { command = 'prettier' },
+  { command = 'black' },
 })
-local linters = require("lvim.lsp.null-ls.linters")
+local linters = require('lvim.lsp.null-ls.linters')
 linters.setup({
-	{ command = "flake8", filetypes = { "python" } },
-	{
-		command = "shellcheck",
-		args = { "--severity", "warning" },
-	},
+  { command = 'flake8', filetypes = { 'python' } },
+  {
+    command = 'shellcheck',
+    args = { '--severity', 'warning' },
+  },
 })
 
 -- -- Additional Plugins <https://www.lunarvim.org/docs/plugins#user-plugins>
 lvim.plugins = {
-	{ "tpope/vim-fugitive" },
-	{
-		"folke/trouble.nvim",
-		cmd = "TroubleToggle",
-	},
-	{
-		"karb94/neoscroll.nvim",
-		event = "WinScrolled",
-		config = function()
-			require("neoscroll").setup({ hide_cursor = false })
-		end,
-	},
-	{
-		"Shatur/neovim-cmake",
-		config = function()
-			local Path = require("plenary.path")
-			Progress = ""
+  { 'tpope/vim-fugitive' },
+  {
+    'folke/trouble.nvim',
+    cmd = 'TroubleToggle',
+  },
+  {
+    'karb94/neoscroll.nvim',
+    event = 'WinScrolled',
+    config = function()
+      require('neoscroll').setup({ hide_cursor = false })
+    end,
+  },
+  {
+    'Shatur/neovim-cmake',
+    config = function()
+      local Path = require('plenary.path')
+      Progress = ''
 
-			require("cmake").setup({
-				cmake_executable = "cmake", -- CMake executable to run.
-				save_before_build = true, -- Save all buffers before building.
-				parameters_file = "neovim.json", -- JSON file to store information about selected target, run arguments and build type.
-				default_parameters = { args = {}, build_type = "Debug" }, -- The default values in `parameters_file`. Can also optionally contain `run_dir` with the working directory for applications.
-				build_dir = tostring(Path:new("{cwd}", "build")), -- Build directory. The expressions `{cwd}`, `{os}` and `{build_type}` will be expanded with the corresponding text values. Could be a function that return the path to the build directory.
-				default_projects_path = tostring(Path:new(vim.loop.os_homedir(), "CmakeProjects")), -- Default folder for creating project.
-				configure_args = { "-D", "CMAKE_EXPORT_COMPILE_COMMANDS=1" }, -- Default arguments that will be always passed at cmake configure step. By default tells cmake to generate `compile_commands.json`.
-				build_args = {}, -- Default arguments that will be always passed at cmake build step.
-				on_build_output = function(lines)
-					-- Get only last line
-					local match = string.match(lines[#lines], "(%[.*%])")
-					if match then
-						Progress = string.gsub(match, "%%", "%%%%")
-					end
-				end,
-				quickfix = {
-					pos = "botright", -- Where to open quickfix
-					height = 5, -- Height of the opened quickfix.
-					only_on_error = false, -- Open quickfix window only if target build failed.
-				},
-				copy_compile_commands = true, -- Copy compile_commands.json to current working directory.
-				dap_configurations = { -- Table of different DAP configurations.
-					lldb_vscode = { type = "lldb", request = "launch" },
-					cppdbg_vscode = { type = "cppdbg", request = "launch" },
-				},
-				dap_configuration = "lldb_vscode", -- DAP configuration to use if the projects `parameters_file` does not specify one.
-				dap_open_command = function(...)
-					require("dap").repl.open(...)
-				end, -- Command to run after starting DAP session. You can set it to `false` if you don't want to open anything or `require('dapui').open` if you are using https://github.com/rcarriga/nvim-dap-ui
-			})
+      require('cmake').setup({
+        cmake_executable = 'cmake', -- CMake executable to run.
+        save_before_build = true, -- Save all buffers before building.
+        parameters_file = 'neovim.json', -- JSON file to store information about selected target, run arguments and build type.
+        default_parameters = { args = {}, build_type = 'Debug' }, -- The default values in `parameters_file`. Can also optionally contain `run_dir` with the working directory for applications.
+        build_dir = tostring(Path:new('{cwd}', 'build')), -- Build directory. The expressions `{cwd}`, `{os}` and `{build_type}` will be expanded with the corresponding text values. Could be a function that return the path to the build directory.
+        default_projects_path = tostring(
+          Path:new(vim.loop.os_homedir(), 'CmakeProjects')
+        ), -- Default folder for creating project.
+        configure_args = { '-D', 'CMAKE_EXPORT_COMPILE_COMMANDS=1' }, -- Default arguments that will be always passed at cmake configure step. By default tells cmake to generate `compile_commands.json`.
+        build_args = {}, -- Default arguments that will be always passed at cmake build step.
+        on_build_output = function(lines)
+          -- Get only last line
+          local match = string.match(lines[#lines], '(%[.*%])')
+          if match then
+            Progress = string.gsub(match, '%%', '%%%%')
+          end
+        end,
+        quickfix = {
+          pos = 'botright', -- Where to open quickfix
+          height = 5, -- Height of the opened quickfix.
+          only_on_error = false, -- Open quickfix window only if target build failed.
+        },
+        copy_compile_commands = true, -- Copy compile_commands.json to current working directory.
+        dap_configurations = { -- Table of different DAP configurations.
+          lldb_vscode = { type = 'lldb', request = 'launch' },
+          cppdbg_vscode = { type = 'cppdbg', request = 'launch' },
+        },
+        dap_configuration = 'lldb_vscode', -- DAP configuration to use if the projects `parameters_file` does not specify one.
+        dap_open_command = function(...)
+          require('dap').repl.open(...)
+        end, -- Command to run after starting DAP session. You can set it to `false` if you don't want to open anything or `require('dapui').open` if you are using https://github.com/rcarriga/nvim-dap-ui
+      })
 
-			vim.keymap.set("n", "<leader>cg", "<CMD>CMake<CR>")
-			vim.keymap.set("n", "<leader>cb", "<CMD>CMake build_all<CR>")
-			vim.keymap.set("n", "<leader>cr", "<CMD>CMake build_and_run<CR>")
-		end,
-	},
+      vim.keymap.set('n', '<leader>cg', '<CMD>CMake<CR>')
+      vim.keymap.set('n', '<leader>cb', '<CMD>CMake build_all<CR>')
+      vim.keymap.set('n', '<leader>cr', '<CMD>CMake build_and_run<CR>')
+    end,
+  },
 }
