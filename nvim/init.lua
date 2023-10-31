459dec02939e42a89d9345e3b5645a7944fd441e
Son Artyom <sai@modumlab.com>
2023-10-31 15:29:07 +0300

    removed nvim for sure

--- nvim/init.lua
+++
@@ -1,4 +0,0 @@
-require('settings')
-require('plugins')
-require('keybinds')
-require('gui-settings')
--- nvim/lua/plugins/term.lua
+++
@@ -1,6 +0,0 @@
-require('toggleterm').setup({
-  start_in_insert = true,
-})
-
-vim.keymap.set({ 'n', 't' }, '<c-m>', '<cmd>ToggleTerm<cr>')
-vim.keymap.del({ 'n', 't' }, '<cr>')
